fba166c43404d85402c6e096850aa88a7bc9ea75
Peter Liu <peter.ty.liu@gmail.com>
2020-06-25 22:00:10 -0400

    Updates

---
+++ precache-manifest.8fb52a8ceda9c031b633a5d7c1c21606.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f903edcfcd333e0fa409885d70e1ea00",
+    "url": "/assignment-15-employee-list/index.html"
+  },
+  {
+    "revision": "0e0a0208ee3647a77bc9",
+    "url": "/assignment-15-employee-list/static/css/main.f2ba12b6.chunk.css"
+  },
+  {
+    "revision": "06754c8a06b257d58f4e",
+    "url": "/assignment-15-employee-list/static/js/2.09ea44b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/assignment-15-employee-list/static/js/2.09ea44b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e0a0208ee3647a77bc9",
+    "url": "/assignment-15-employee-list/static/js/main.bf9e3ac5.chunk.js"
+  },
+  {
+    "revision": "d18a658482841c594588",
+    "url": "/assignment-15-employee-list/static/js/runtime-main.f219780f.js"
+  }
+]);
